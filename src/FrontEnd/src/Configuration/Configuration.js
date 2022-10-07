dc7c347d7150da60abba936527e4a32281a32548
Cppshizoid <alex.krivoschecov@gmail.com>
2022-10-07 23:40:53 +0700

    Delete src/FrontEnd/src directory

--- src/FrontEnd/src/Configuration/Configuration.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  InsertNote: 'https://localhost:5001/api/ToDoList/InsertNote',
-  GetNote: 'https://localhost:5001/api/ToDoList/GetNote',
-  GetNoteById:'https://localhost:5001/api/ToDoList/GetNoteById?id=',
-  UpdateNote: 'https://localhost:5001/api/ToDoList/UpdateNote',
-  DeleteNote: 'https://localhost:5001/api/ToDoList/DeleteNote?id=',
-}
